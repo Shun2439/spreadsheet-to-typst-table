8c926f69c91b44d28a3bfc9a4e0281da67b61e3e
Shun2439 <shunta.kobayashi24@gmail.com>
2025-07-30 20:37:31 +0900

    feat: from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
-use std::path::Path;
+use std::{fs::File, io::{self, Read}, path::Path};
+
+fn getline() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer.to_string()
+}
 
 fn main() {
-    let path = Path::new("demo.txt");
-    let display = path.display();
-    println!("Path: {}", display);
+    // let path = Path::new("demo.txt");
+    // let display = path.display();
+    // println!("Path: {}", display);
+
+    // let mut file = match File::open(&path) {
+    //     Err(why) => panic!("couldn't open {}: {}", display, why),
+    //     Ok(file) => file,
+    // };
+
+    // let mut s = String::new();
+    // match file.read_to_string(&mut s) {
+    //     Err(why) => panic!("couldn't read {}: {}", display, why),
+    //     Ok(_) => println!("{} contains:\n{}", display, s),
+    // }
+
+    print!("Input amout of rows: ");
+    let n = getline().trim().parse::<i32>().unwrap();
+    println!("Input string: ");
+
+    let mut s = String::new();
+
+    for _ in 0..n {
+        let l = getline();
+        s.push_str(&l.as_str());
+    }
+
+    print!("[");
+    for i in s.chars() {
+        if i == ' ' || i == '　' || i == '\t' {
+            print!("], [");
+            continue;
+        }
+        else if i == '\n' {
+           print!("],\n[");
+           continue;
+        }
+        print!("{}", i);
+    }
 }
